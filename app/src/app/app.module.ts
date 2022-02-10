7f6e0cae253e7ae2e3bf95ca0b18362bc1fe6c54
Jorge <joalru01@floridauniversitaria.es>
2022-02-10 13:37:32 +0100

    afadima

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './component/cards/cards.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
