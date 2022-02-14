a71dda29d6d9262d530a13ad76bc54c513ba3753
nacachiri <nachocalomarde@gmail.com>
2022-02-15 00:09:46 +0100

    creado vista subir imagen

--- app/src/app/vistas/upload-img/upload-img.component.ts
+++ app/src/app/vistas/upload-img/upload-img.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-upload-img',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadImgComponent implements OnInit {
 
+  apiUrl:string  = 'http://35.180.22.126:8000/api/';
+  formData:any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fileEvent(fileInput: any){
+    let file = (<HTMLInputElement>fileInput.target).files
+    if (file == null) {
+      return;
+    }
+
+    this.formData = new FormData();
+    this.formData.append("image", file[0]);
+  }
+
+  uploadFile(){
+    axios.post(this.apiUrl + 'noticias/new', this.formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then((response) =>{
+      console.log(response.data);
+    })
   }
 
 }
