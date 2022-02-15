1903b7018f1d16eb1aa721d720f877a8ed8453bc
Nacachiri <nachocalomarde@gmail.com>
2022-02-15 10:25:38 +0100

    Almacenado token

--- app/src/app/vistas/login/login.component.ts
+++ app/src/app/vistas/login/login.component.ts
@@ -8,25 +8,26 @@ import axios from 'axios';
 })
 export class LoginComponent implements OnInit {
 
-  email : string;
-  password : string;
+  email : string = '';
+  password : string = '';
+  codeError: number = 0;
+  errorMessage: string = '';
 
-  constructor() {
-    this.email  = "";
-    this.password = "";
-  }
+  constructor() {}
   
   getUser() {
   
     axios.post('http://35.180.22.126:8000/api/login', {
-      email: this.email,
-      password: this.password 
+        email: this.email,
+        password: this.password 
     })
     .then((response) => {
-      console.log(response);
+        sessionStorage.setItem('token', response.data.token)
+        this.codeError = 0;
     })
     .catch((error) => {
-      console.log(error);
+        this.codeError = error.response.data.code;
+        this.errorMessage = error.response.data.message;
     });
   }
 
