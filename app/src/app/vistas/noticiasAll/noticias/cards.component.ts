8bad542363603e534b5ce219d6fb9ae5f5c1a146
nacachiri <nachocalomarde@gmail.com>
2022-02-16 00:44:49 +0100

    noticias cards

--- app/src/app/vistas/noticiasAll/noticias/cards.component.ts
+++ app/src/app/vistas/noticiasAll/noticias/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
+  apiUrl:string  = 'http://35.180.22.126:8000/api/';
+  imagenesUrl:string  = 'http://35.180.22.126:8000/';
+  noticias:Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getAllNoticias()
+  }
+
+  getAllNoticias(){
+    axios.get(this.apiUrl + 'noticias').then((response) =>{
+      console.log(response.data);
+      this.noticias = response.data;
+    })
   }
 
 }
