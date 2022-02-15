8be94eaa4dd3debbe040aadb9637a7f10bf8fb02
Nacachiri <nachocalomarde@gmail.com>
2022-02-15 10:00:23 +0100

    borrado archivo de login

--- app/src/app/vistas/login/login.component.ts
+++ app/src/app/vistas/login/login.component.ts
@@ -7,37 +7,30 @@ import axios from 'axios';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    public archivos : any = [];
-    email : string;
-    password : string;
-    image : string;
+
+  email : string;
+  password : string;
+
   constructor() {
-    this.image = "";
     this.email  = "";
     this.password = "";
   }
   
-   getUser() {
+  getUser() {
   
     axios.post('http://35.180.22.126:8000/api/login', {
       email: this.email,
       password: this.password 
-      
     })
-    .then(function (response) {
+    .then((response) => {
       console.log(response);
     })
-    .catch(function (error) {
-      
+    .catch((error) => {
       console.log(error);
     });
   }
+
   ngOnInit(): void {
   }
-  capturarFile(event: any){
-    const archivoCapturado = event.target.files[0];
-    this.archivos.push(archivoCapturado);
-    //console.log(event.target.files);
 
-  }
 }
